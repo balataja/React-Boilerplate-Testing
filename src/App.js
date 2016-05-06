26c1cbfadc9ecb9ae3b36d5018d460dac7f0791b
Balata <JBalata@geico.com>
2016-05-06 10:43:55 -0400

    Editing React components 2

--- src/App.js
+++ src/App.js
@@ -3,20 +3,68 @@ ReactDOM.render(
     document.getElementById('main')
 );
 
-/*import React from "react"
+import React from "react"
 
-export default class BugList extends React.Component
+class BugList extends React.Component
 {
     render()
     {
-        return(<div>Place Holder Text!</div>);
+        return(
+            <div>
+                <BugFilter />
+                <BugTable />
+                <BugAdd />
+            </div>);
     }
-}*/
+}
 
-var BugList = React.createClass({
-  render: function() {
-    return (
-      <div>The bug list would come here.</div>
-    )
-  }
-});
+class BugFilter extends React.Component
+{
+    render()
+    {
+        return(<div>Bug Filter!</div>);
+    }
+}
+class BugTable extends React.Component
+{
+    render()
+    {
+        return(
+            <div>
+                <table>
+                    <tr>
+                        <th>ID</th>
+                        <th>Status</th>
+                        <th>Priority</th>
+                        <th>Owner</th>
+                        <th>Title</th>
+                    </tr>
+                    <BugRow id={1} priority="P1" status="Open" owner="Ravan" title="App crashes on open" />
+                    <BugRow id={2} priority="P2" status="New" owner="Eddie" title="Misaligned border on panel" />
+                </table>
+            </div>);
+    }
+}
+class BugAdd extends React.Component
+{
+    render()
+    {
+        return(<div>Bug Add!</div>);
+    }
+}
+class BugRow extends React.Component
+{
+    render()
+    {
+        return(
+            <div>
+                <tr>
+                    <td>{this.props.id}</td>
+                    <td>{this.props.status}</td>
+                    <td>{this.props.priority}</td>
+                    <td>{this.props.owner}</td>
+                    <td>{this.props.title}</td>
+                </tr>
+            </div>);
+    }
+}
