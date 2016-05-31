4fc1d20dc26e153b21135583066b32f93b55049f
Balata <JBalata@geico.com>
2016-05-31 15:19:59 -0400

    added react-router with nav bar. need to figure out how to use react-router includes with es6.

--- src/index.js
+++ src/index.js
@@ -1,50 +1,31 @@
 import react from 'react';
 import reactDOM from 'react-dom';
+import App from './App';
 import Home from './Home';
 import Random from './Random';
 import About from './About';
 import Guides from './Guides';
 import Error from './Error';
-import Router from 'react-router';
-import Route from 'react-router';
-import Redirect from 'react-router';
+/*import { Router } from 'react-router';
+import { Route } from 'react-router';
+import { Redirect } from 'react-router';*/
+var ReactRouter = require('react-router');
+var Router = ReactRouter.Router;
+var Route = ReactRouter.Route;
+var Redirect = ReactRouter.Redirect;
+var IndexRoute = ReactRouter.IndexRoute;
+var browserHistory = ReactRouter.browserHistory;
+var hashHistory = ReactRouter.hashHistory;
 
-/*export default React.createClass({
-  render(){
-    return(
-      <Home />,
-      (
-        <Router>
-          <Route path="/Home" component={Home} />
-          <Redirect from="/" to="/Home" />
-          <Route path="/Random" component={Random} />
-          <Route path="/About" component={About} />
-          <Route path="/Guides" component={Guides} />
-          <Route path="*" component={Error} />
-        </Router>
-      ),
-      document.getElementById('main')
-    )
-  }
-});*/
-
-ReactDOM.render(
-    <Home />,
-    (
-      <Router>
-        <Route path="/Home" component={Home} />
-        <Redirect from="/" to="/Home" />
-        <Route path="/Random" component={Random} />
-        <Route path="/About" component={About} />
-        <Route path="/Guides" component={Guides} />
-        <Route path="*" component={Error} />
-      </Router>
-    ),
-    document.getElementById('main')
+reactDOM.render((
+  <Router history={hashHistory} >
+    <Route path="/" component={App} >
+      <IndexRoute component={Home} />
+      <Route path="Home" component={Home} />
+      <Redirect from="/" to="/Home" />
+      <Route path="Random" component={Random} />
+      <Route path="About" component={About} />
+      <Route path="Guides" component={Guides} />
+    </Route>
+  </Router>), document.getElementById("main")
 );
-
-//This Works, check for issue with react-router
-/*ReactDOM.render(
-  <Home />,
-  document.getElementById('main')
-);*/
