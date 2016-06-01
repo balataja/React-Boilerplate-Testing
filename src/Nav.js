d6ea4b7164412a075389f4fe6fafda8ee6e09ae7
Balata <JBalata@geico.com>
2016-06-01 16:54:33 -0400

    updating nav to work with react-router-bootstrap

--- src/Nav.js
+++ src/Nav.js
@@ -1,17 +1,32 @@
 import React from 'react';
 import { Link } from 'react-router';
 import NavLink from './NavLink';
+import ReactBootstrap from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
+import { Nav } from 'react-bootstrap';
+import { NavItem } from 'react-bootstrap';
+import { Navbar } from 'react-bootstrap';
 
 export default React.createClass({
     render() {
         return (
             <section className="header">
-                <h1><Link to="/"> James' Blog</Link></h1>
-                <nav className="nav-menu">
-                    <li><NavLink to="/Random">Link Name</NavLink>Random</li>
-                    <li><NavLink to="/About">Link Name</NavLink>About</li>
-                    <li><NavLink to="/Guides">Link Name</NavLink>Guides</li>
-                </nav>
+                <Navbar >
+                    <Navbar.Header>
+                        <Navbar.Brand>
+                            <a href="/">James' Blog</a>
+                        </Navbar.Brand>
+                        <Navbar.Toggle />
+                    </Navbar.Header>
+                    <Navbar.Collapse>
+                        <Nav>
+                            <LinkContainer to={{pathname:'/Random'}}><NavItem eventKey={1}>Random</NavItem></LinkContainer>
+                            <LinkContainer to={{pathname:'/About'}}><NavItem eventKey={2} href="/About">About</NavItem></LinkContainer>
+                            <LinkContainer to={{pathname:'/Guides'}}><NavItem eventKey={3} href="/Guides">Guides</NavItem></LinkContainer>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Navbar>
             </section>
         )
     }
